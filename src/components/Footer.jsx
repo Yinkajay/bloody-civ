32974176f1b2655e5c0716dfb50ce3dbd5f94be2
Adeyinka Junaid <yinkajunn@gmail.com>
2024-01-08 20:19:35 +0100

    Add footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,33 @@
+import Link from 'next/link'
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <footer>
+            <section>
+                <div>
+                    <h2>Bloody</h2>
+                    <img src="" alt="" />
+                    <h2>Civilian</h2>
+                </div>
+                <div>
+                    <section>
+                        <Link href='/'>X</Link>
+                        <Link href='/'>Instagram</Link>
+                        <Link href='/'>Snapchat</Link>
+                    </section>
+                    <section>
+                        
+                    </section>
+                </div>
+                <div>
+                    <img src="" alt="" />
+                    <p>deisgned and developed with </p>
+                </div>
+            </section>
+            <p>Fan Site for bloody civilian @ 2024</p>
+        </footer>
+    )
+}
+
+export default Footer
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,3 +1,4 @@
+import Footer from "./Footer";
 import Navbar from "./Navbar";
 
 // export default function Layout({ children }) {
@@ -11,6 +12,7 @@ const Layout = ({ children }) => {
       <div>
         <Navbar />
         {children}
+        <Footer />
       </div>
     );
   };
