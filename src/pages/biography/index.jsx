7e0ced9f2e8570eb40ec687d172e245111d010c1
Adeyinka Junaid <yinkajunn@gmail.com>
2024-09-11 09:33:40 +0100

    Add more images on biography page

--- src/pages/biography/index.jsx
+++ src/pages/biography/index.jsx
@@ -5,6 +5,13 @@ import BiographyStyles from './index.module.css'
 
 import bioImage from '../../../assets/biography/bioPicture.svg'
 import megaphone from '../../../assets/biography/megaphone.svg'
+import videoThumb from '../../../assets/biography/videoThumb.svg'
+
+import Youtube from '../../../assets/biography/Youtube.svg'
+import Snapchat from '../../../assets/biography/Snapchat.svg'
+import Instagram from '../../../assets/biography/Instagram.svg'
+import Twitter from '../../../assets/biography/Twitter.svg'
+import Pencil from '../../../assets/biography/Pencil.svg'
 
 export default function biography() {
   return (
@@ -19,23 +26,29 @@ export default function biography() {
         <p>Nigerian singer-songwriter <br /> and record producer</p>
       </header>
 
-      <div>
-        <img />
+      <div className={BiographyStyles.videoArea}>
+        <Image src={videoThumb} />
         <p>Born and raised in Abuja, Nigeria, Bloody Civilian is a genre-defying singer, whose penchant for storytelling manifests in her authentic, unbridled, and poetic lyricism. Bloody Civilian grew up in a supportive family system, to a mother who was medical doctor and an engineer father, who played the bass guitar with his band in the ‘80s. Biologically the first born, Bloody Civilian’s household included many older siblings, and with this background the singer, songwriter and producer gained exposure to a broad range of musical tastes.
         </p>
       </div>
 
-      <div>
+      <div className={BiographyStyles.randomArea}>
         <div>
-          <div></div>
-          <img />
+          <div>
+            <Image src={Youtube} />
+            <Image src={Snapchat} />
+            <Image src={Instagram} />
+            <Image src={Twitter} />
+          </div>
+          <Image src={Pencil} />
         </div>
         <div>
-          <img />
+          <Image  />
+          <Image />
         </div>
       </div>
 
-      <div>
+      <div className={BiographyStyles.preVisuals}>
         <h2>She’s got that finesse</h2>
         <p>“I love writing stories and telling stories and sharing what is on my mind without consequences,” Bloody Civilian says of her expressive experiential music. Initially, delving into the art as a form of poetic expression, Bloody Civilian’s parents were her first audience, to whom she’d write and perform songs, though soon enough she’d take to the public stages of church, talent shows and school performances.</p>
         <p>Her repertoire grew quickly, with the help of Audacity, where a 12-year-old Bloody Civilian began studying music production, which has now evolved into a stunning self-produced debut single “How To Kill A Man.”</p>
